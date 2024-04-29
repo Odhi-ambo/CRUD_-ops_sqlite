f9a9b440bc2328d9f21d209ca2087fb6b0ed80c1
Odhi-ambo <michaelderrick605@gmail.com>
2024-04-29 11:45:50 +0300

    initialised sqlite

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:crud_ops_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+@override
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
